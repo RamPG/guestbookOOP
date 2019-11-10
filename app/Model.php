2e946b9a19dfd52965c2f4738bd08c69ae958c0b
RamPG <vlasovnew@yandex.ru>
2019-11-10 13:12:35 +0300

    second commit

---
+++ app/Model.php
@@ -0,0 +1,16 @@
+<?php
+class Model
+{
+public function __construct()
+{
+$this->data_get = explode("| ", file_get_contents("book.txt"));
+}
+
+public static function putContent($controller)
+{
+$data_put = $controller->name . " " . $controller->text . " " . $controller->date . "| ";
+file_put_contents("book.txt", $data_put, FILE_APPEND);
+
+}
+}
+?>
